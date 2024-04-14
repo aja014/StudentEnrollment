49272ac651316859ad5e57ab48faec490753011a
aja014 <142507287+aja014@users.noreply.github.com>
2024-04-14 23:04:34 +0800

    jus some practice

--- Practice/Mainsource.cpp
+++ Practice/Mainsource.cpp
@@ -5,71 +5,124 @@
 #include <conio.h>
 #include <sstream>
 
+#include "Graphics.h"
+#include <Windows.h>
+
 #include "add.cpp"
 #include "list.cpp"
 
 using namespace std;
 
 
+//int main() {
+//
+//
+//	// ios::app - Append/update
+//	// ios::in - Reading
+//	// ios::out - writing/creating
+//
+//	int cntr = 1;
+//Q:
+//	coorxy(5, 5); cout << "[ ] Add Employee";
+//	coorxy(5, 6); cout << "[ ] See the list";
+//	coorxy(5, 7); cout << "[ ] Exit";
+//
+//W:
+//	for (int a = 5; a <= 7; a++) {
+//		coorxy(6, a); cout << " ";
+//	}
+//
+//	if (cntr == 1) {
+//		coorxy(6, 5); cout << "#";
+//	}else if (cntr == 2) {
+//		coorxy(6, 6); cout << "#";
+//	}else if (cntr == 3) {
+//		coorxy(6, 7); cout << "#";
+//	}
+//	
+//	switch (_getch()) {
+//	case 224:
+//		switch (_getch()) {
+//		case 72:
+//			if (cntr == 1) cntr = 3;
+//			else cntr--;
+//			break;
+//		case 80:
+//			if (cntr == 3) cntr = 1;
+//			else cntr++;
+//			break;
+//		}
+//		goto W;
+//		break;
+//	case 13:
+//		if (cntr == 1) {
+//			addemp();
+//		}
+//		else if (cntr == 2) {
+//			vw();
+//		}
+//		else if (cntr == 3) {
+//			// exit
+//			system("cls");
+//			exit(0);
+//		}
+//	default:
+//		goto Q;
+//		break;
+//	}
+//
+//
+//
+//
+//	//coorxy(0, 29); system("pause");
+//}
+
+
+#include "graphics.h"
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
 int main() {
+    initgraph(); // Initialize graphics environment
+    int x = 10;
+    int y = 10;
 
 
-	// ios::app - Append/update
-	// ios::in - Reading
-	// ios::out - writing/creating
+    while (true) {
+        clrscr();
+        circle(10, 10, 20);
+        y += 2;
+        if (y == 100) {
+            break;
+        }
+        Sleep(1000);
+    }
 
-	int cntr = 1;
-Q:
-	coorxy(5, 5); cout << "[ ] Add Employee";
-	coorxy(5, 6); cout << "[ ] See the list";
-	coorxy(5, 7); cout << "[ ] Exit";
+    //int x = 100; // Initial x-coordinate of the circle
+    //int y = 100; // Initial y-coordinate of the circle
+    //int radius = 20; // Radius of the circle
 
-W:
-	for (int a = 5; a <= 7; a++) {
-		coorxy(6, a); cout << " ";
-	}
+    //while (true) { // Animation loop
+    //    clrscr(); // Clear the screen
+    //    circle(x, y, radius); // Draw the circle
 
-	if (cntr == 1) {
-		coorxy(6, 5); cout << "#";
-	}else if (cntr == 2) {
-		coorxy(6, 6); cout << "#";
-	}else if (cntr == 3) {
-		coorxy(6, 7); cout << "#";
-	}
-	
-	switch (_getch()) {
-	case 224:
-		switch (_getch()) {
-		case 72:
-			if (cntr == 1) cntr = 3;
-			else cntr--;
-			break;
-		case 80:
-			if (cntr == 3) cntr = 1;
-			else cntr++;
-			break;
-		}
-		goto W;
-		break;
-	case 13:
-		if (cntr == 1) {
-			addemp();
-		}
-		else if (cntr == 2) {
-			vw();
-		}
-		else if (cntr == 3) {
-			// exit
-			system("cls");
-			exit(0);
-		}
-	default:
-		goto Q;
-		break;
-	}
+    //    // Update coordinates for next frame
+    //    x += 1;
+    //    y += 1;
 
+    //    // Check if the circle goes beyond the screen boundaries
+    //    if (x > getmaxx() || y > getmaxy()) {
+    //        x = 100; // Reset x-coordinate
+    //        y = 100; // Reset y-coordinate
+    //    }
 
+    //    Sleep(50); // Delay for smooth animation
+    //}
 
+    //return 0;
 
-	//coorxy(0, 29); system("pause");
+    coorxy(0, 29);
 }
+
+	
